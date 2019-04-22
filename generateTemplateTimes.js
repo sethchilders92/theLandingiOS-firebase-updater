6d5e409948668036a7b7efb6d78be6c04c54503b
Seth Childers <sethchilders92@gmail.com>
2019-04-22 15:00:28 -0700

    Updated code and documentation

--- generateTemplateTimes.js
+++ generateTemplateTimes.js
@@ -4,12 +4,21 @@
  * You also need to delete the times included that aren't used.
  * 
  * I run this and pipe its contents into a json file.
- * e.g. node times.js > times.json
+ * e.g. node generateTemplateTimes.js > times.json
  */
+
+// the object which is essentially the json file
+ let jsonFile = {
+    all_times: []
+}
+
+// we will use military time so we don't have to deal with 'am' or 'pm' and the shuttle only runs until 17:00 (aka 05:00pm)
 for (let hour = 7; hour < 17; hour++) {
     for (let minute = 0; minute < 60; minute += 5) {
         let hourString = `${hour}`;
         let minuteString = `${minute}`;
+
+        // if the hour or minute is less than ten, tack on a leading zero for the time (e.g. 07:05 instead of 7:5)
         if (hour < 10) {
             hourString = `0${hour}`;
         }
@@ -17,14 +26,16 @@ for (let hour = 7; hour < 17; hour++) {
             minuteString = `0${minute}`
         }
         let time = `${hourString}:${minuteString}`;
-        let output = 
-        `{
-            "start": "",
-            "end": "",
-            "days": "",
-            "time": "${time}"
-        },
-        `;
-        console.log(output);
+        let shuttleTime = {
+            start: '',
+            end: '',
+            days: '',
+            time: time
+        };
+
+        // put the new time onto the array of times
+        jsonFile.all_times.push(shuttleTime);
     }
 }
+
+console.log(JSON.stringify(jsonFile))
